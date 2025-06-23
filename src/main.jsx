405e4d1b01a13556a7ac5c132407b99ca39212dc
k-web-tech <kamrulislamapurba@gmail.com>
2025-06-23 12:06:47 +0600

    Bassic setup Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+      {/* <Route path="about" element={<About />} />
+
+      <Route element={<AuthLayout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+
+      <Route path="concerts">
+        <Route index element={<ConcertsHome />} />
+        <Route path=":city" element={<City />} />
+        <Route path="trending" element={<Trending />} />
+      </Route> */}
+    </Routes>
+  </BrowserRouter>
+);
